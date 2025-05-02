95c602d3d6cbcf24cba81929df307c9b1593c57e
Joel Bleeker <joelb@topdesk.com>
2025-05-02 16:08:27 +0200

    add lowpass and volume control

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 document.querySelector('#playButton').addEventListener('click', () => playNoise())
 
+
+
+document.querySelector('#volume').addEventListener('change', () => {
+	if (gain) {
+		gain.gain.value = parseFloat(document.querySelector('#volume').value)
+	}
+})
+document.querySelector('#lowpass').addEventListener('change', () => {
+	if (lowpass) {
+		lowpass.frequency.value = parseInt(document.querySelector('#lowpass').value)
+	}
+})
 // Pink noise filter coefficients (approximated)
 // These coefficients are adapted from standard pink noise filter approximations
 // Reference: Paul Kellet's pink noise filter
 const feedforward = [0.049922035, -0.095993537, 0.050612699, -0.004408786];
 const feedback = [1.0, -2.494956002, 2.017265875, -0.522189400];
 
-
 let currentNoise = undefined
 let playing = false
+
+let lowpass = undefined
+let gain = undefined
 
 function playNoise() {
   const time = 60 * 60
@@ -25,6 +39,12 @@ function playNoise() {
 
   const audioContext = new AudioContext();
 
+  lowpass = audioContext.createBiquadFilter();
+  lowpass.type = 'lowpass'
+  lowpass.frequency.value = parseInt(document.querySelector('#lowpass').value)
+  
+  gain = audioContext.createGain()
+  
   const pinkFilter = audioContext.createIIRFilter(feedforward, feedback);
 
   const bufferSize = audioContext.sampleRate * 1000
@@ -45,7 +65,9 @@ function playNoise() {
   });
   
   currentNoise = noise;
-  noise.connect(pinkFilter).connect(audioContext.destination);
+  noise.connect(pinkFilter).connect(lowpass).connect(gain).connect(audioContext.destination);
   noise.start(0,0,time);
   playing = true
+  
+
 }
