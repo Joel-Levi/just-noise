5baf0168eb48a51813e322ce0d3e687512e7425f
Joel Bleeker <joelb@topdesk.com>
2025-05-05 13:54:12 +0200

    Storage for lowpass and volume

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
+function mapRange(value, oldMin, oldMax, newMin, newMax) {
+	return ((value - oldMin) * (newMax - newMin) / (oldMax - oldMin)) + newMin;
+  }
+
+if (localStorage.key('volume')) {
+	document.querySelector('#volume').value = localStorage.getItem('volume')
+}
+if (localStorage.key('lowpass')) {
+	document.querySelector('#lowpass').value = localStorage.getItem('lowpass')
+}
+
 document.querySelector('#playButton').addEventListener('click', () => playNoise())
 
-
-
 document.querySelector('#volume').addEventListener('change', () => {
+	const value = parseInt(document.querySelector('#volume').value)
+	localStorage.setItem('volume', value.toString())
 	if (gain) {
-		gain.gain.value = parseFloat(document.querySelector('#volume').value)
+		gain.gain.value = mapRange(value, 0, 100, 0, 2)
 	}
 })
+
 document.querySelector('#lowpass').addEventListener('change', () => {
+	const value = parseInt(document.querySelector('#lowpass').value)
+	localStorage.setItem('lowpass', value.toString())
 	if (lowpass) {
-		lowpass.frequency.value = parseInt(document.querySelector('#lowpass').value)
+		lowpass.frequency.value = value
 	}
 })
 // Pink noise filter coefficients (approximated)
@@ -41,9 +55,19 @@ function playNoise() {
 
   lowpass = audioContext.createBiquadFilter();
   lowpass.type = 'lowpass'
-  lowpass.frequency.value = parseInt(document.querySelector('#lowpass').value)
+  
+  if (!localStorage.key('lowpass')) {
+    localStorage.setItem('lowpass', '150')
+  }
+  
+  lowpass.frequency.value = parseInt(localStorage.getItem('lowpass'))
+  
+  if (!localStorage.key('gain')) {
+    localStorage.setItem('volume', '50')
+  }
   
   gain = audioContext.createGain()
+  gain.value = mapRange(parseInt(localStorage.getItem('volume')), 0, 100, 0, 2)
   
   const pinkFilter = audioContext.createIIRFilter(feedforward, feedback);
 
